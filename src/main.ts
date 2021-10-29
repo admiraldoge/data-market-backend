04c82381e018d6a0e56da0b6dcfd3b66e5d98980
Maximilian  Vino <admiraldoge@Maximilians-Mac-mini.local>
2021-10-29 01:16:31 -0400

    Adding documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Data Market')
+    .setDescription('This is the documentation for Data Market APIs')
+    .setVersion('0.1')
+    .addTag('data')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
   app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   await app.listen(8080);
